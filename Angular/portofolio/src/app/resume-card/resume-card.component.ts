c1fa17cf0e9fe3711aec0d3683b4f44e8120106f
Stefy237 <ngueganglaura@gmail.com>
2025-01-25 01:00:13 +0100

    init projet global

---
+++ Angular/portofolio/src/app/resume-card/resume-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resume-card',
+  imports: [],
+  templateUrl: './resume-card.component.html',
+  styleUrl: './resume-card.component.scss'
+})
+export class ResumeCardComponent {
+
+}
